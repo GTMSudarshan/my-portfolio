e51dba3d2249c7956b0440a419a9092c09babb68
Sudarshan Gautam <89502760+GTMSudarshan@users.noreply.github.com>
2023-04-04 21:59:47 +0545

    first portfolio of gtm :D

---
+++ index.js
@@ -0,0 +1,17 @@
+/*Animation*/
+document.addEventListener("DOMContentLoaded", function(event) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+  
+    const hiddenElement = document.querySelector('.hidden');
+    observer.observe(hiddenElement);
+  });
+  
+  
